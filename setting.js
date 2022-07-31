97de7321c4e281f9b56f49e8ed9b8858a849158c
solixbek03 <clashbommm@gmail.com>
2022-08-01 04:05:51 +0500

    add commit

---
+++ setting.js
@@ -0,0 +1,16 @@
+var age = parseInt(prompt("Yoshingiz nechida?").trim(), 10);
+
+
+if (isNaN(age)) {
+  document.querySelector(".text").textContent = "Iltimos, son kiriting.";
+} else if (age >= 100) {
+  document.querySelector(".text").textContent = "Iltimos, oyin qilmay yoshingizni ayting.";
+} else if (age >= 60) {
+  document.querySelector(".text").textContent = "Siz 1000 so'm pul to'laysiz.";
+} else if (age >= 20) {
+  document.querySelector(".text").textContent = "Siz 2000 so'm pul to'laysiz.";
+} else if (age >= 7) {
+  document.querySelector(".text").textContent = "Siz 1400 so'm pul to'laysiz";
+} else if (age < 7) {
+  document.querySelector(".text").textContent = "Sizga chipta bepul.";
+};
